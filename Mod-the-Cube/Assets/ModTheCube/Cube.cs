0af228cc7600b2da1fae2ea0cae6e24f9ff51a16
Agustin Rosso <60651699+Hazard103508@users.noreply.github.com>
2021-08-25 21:10:21 -0300

    .
    .

--- Mod-the-Cube/Assets/ModTheCube/Cube.cs
+++ Mod-the-Cube/Assets/ModTheCube/Cube.cs
@@ -1,23 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cube : MonoBehaviour
 {
+    public Text text;
     public MeshRenderer Renderer;
-    
+
+    public Vector3 rgb;
+    public Vector3 direction;
+
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
-        
-        Material material = Renderer.material;
-        
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+        //transform.position = Vector3.up;
+        transform.localScale = Vector3.one * 2f;
+
+        rgb = Vector3.zero;
+        direction = Vector3.one;
+
+        InvokeRepeating("ChangeColor", 0, 0.05f);
     }
-    
+
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(Vector3.one * 10.0f * Time.deltaTime);
+    }
+
+    private void ChangeColor()
+    {
+        Material material = Renderer.material;
+
+        rgb.x = material.color.r + Random.Range(0f, 1 / 64f) * direction.x;
+        rgb.y = material.color.g + Random.Range(0f, 1 / 64f) * direction.y;
+        rgb.z = material.color.b + Random.Range(0f, 1 / 64f) * direction.z;
+
+        if (rgb.x > 1 || rgb.x < 0)
+        {
+            rgb.x = Mathf.Clamp01(rgb.x);
+            direction.x *= -1;
+        }
+        if (rgb.y > 1 || rgb.y < 0)
+        {
+            rgb.y = Mathf.Clamp01(rgb.y);
+            direction.y *= -1;
+        }
+        if (rgb.z > 1 || rgb.z < 0)
+        {
+            rgb.z = Mathf.Clamp01(rgb.z);
+            direction.z *= -1;
+        }
+
+        text.text = $"R:{rgb.x}\nG:{rgb.y}\nB:{rgb.z}";
+        material.color = new Color(rgb.x, rgb.y, rgb.z, 1);
     }
 }
